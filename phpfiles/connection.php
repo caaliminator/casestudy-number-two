0c7af1731bbc06614906a74c1b6564cd93911bb4
caaliminator <106441187+caaliminator@users.noreply.github.com>
2022-09-02 23:04:07 +0800

    Update connection.php

--- phpfiles/connection.php
+++ phpfiles/connection.php
@@ -1,6 +1,7 @@
-
 <?php
-$server = "localhost";
-$username = "root";
+$server = "us-cdbr-east-06.cleardb.net";
+$username = "bee2eef6c5f3c7";
+$dbname = "heroku_275cd760e77c089";
+$password = "bd97b44a";
 
-$conn = new PDO("mysql:host=$server;dbname=mars_db", $username);
+$conn = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
